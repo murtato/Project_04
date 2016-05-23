0a2dcf77ff047fed95e954031fccfb6ec01a84b5
Victor Tran <murtato@gmail.com>
2016-05-23 09:33:21 -0700

    figuring out ui routers before doing backend

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+angular
+  .module('refugeeApp', ['ui.router'])
+  .config(MainRouter);
+
+MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+function MainRouter ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "home.html",
+    });
+    .state('archive', {
+      url: "/archive",
+      templateUrl: "archive.html",
+    });
+
+    $urlRouterProvider.otherwise('/');
+}
+
